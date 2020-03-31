73d192aa46a01fdbdc42e65736d1aeea12071f2c
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2020-03-31 13:48:13 +0200

    fix

---
+++ DRRecordREST/Models/MusicContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DRRecordREST.Models
+{
+    public class MusicContext : DbContext
+    {
+        public MusicContext(DbContextOptions<MusicContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Record> Records { get; set; } 
+
+    }
+}
