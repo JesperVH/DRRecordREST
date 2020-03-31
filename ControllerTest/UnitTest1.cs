63aac151c9eed254659c330a3cb861eb22306465
Jesper Voldby Hansen <jesp58c0@edu.easj.dk>
2020-03-31 13:39:41 +0200

    Lavet test til specifik get metode

--- ControllerTest/UnitTest1.cs
+++ ControllerTest/UnitTest1.cs
@@ -9,11 +9,11 @@ namespace ControllerTest
     [TestClass]
     public class UnitTest1
     {
-        private static readonly List<Record> records = new List<Record>()
-        {
-            new Record("First Night Back in Town", "Ye Banished Privateers", 1.07, 2017),
-            new Record("Meteora", "Linkin Park", 3.44, 2011)
-        };
+        //private static readonly List<Record> records = new List<Record>()
+        //{
+        //    new Record("First Night Back in Town", "Ye Banished Privateers", 1.07, 2017),
+        //    new Record("Meteora", "Linkin Park", 3.44, 2011)
+        //};
         [TestMethod]
         public void TestGetAll()
         {
@@ -22,10 +22,20 @@ namespace ControllerTest
 
             var result = controller.GetRecords() as List<Record>;
 
-            Assert.AreEqual(records.Count, result.Count);
+            Assert.AreEqual(2, result.Count);
         }
 
-        
+        [TestMethod]
+        public void TestGetRecordByTitle()
+        {
+
+            var controller = new RecordsController();
+
+            var result = controller.GetByTitle("Castle of Glass");
+
+            Assert.AreEqual("Castle of Glass", result.Title);
+            Assert.AreEqual("Linkin Park", result.Artist);
+        }
 
     }
 }
